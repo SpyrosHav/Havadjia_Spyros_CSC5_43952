281a7c58865fe7289712d5dab33a6a7f9289f1be
SpyrosHav <shavadjia@student.rcc.edu>
2015-02-25 10:44:02 -0800

    Signed-off-by: SpyrosHav <shavadjia@student.rcc.edu>

---
+++ Lab/HelloWorld2ndF/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on February 25, 2015, 10:41 AM
+ *      Purpose:Wasnt Satisfied With One F
+ *              I wanted Another
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
