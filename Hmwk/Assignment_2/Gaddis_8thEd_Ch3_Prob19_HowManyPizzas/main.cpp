a3e31fd37b43628c832d35454c4fc6bfeef9616f
spyroshav <shavadjia@student.rcc.edu>
2015-03-18 10:02:15 -0700

    Signed-off-by: spyroshav <shavadjia@student.rcc.edu>

---
+++ Hmwk/Assignment_2/Gaddis_8thEd_Ch3_Prob19_HowManyPizzas/main.cpp
@@ -0,0 +1,43 @@
+/* 
+ * File:   main.cpp
+ * Author: Spyros Havadjia
+ *      Purpose: Assignment 2
+ *          How Many Pizzas
+ * Created on March 17, 2015, 11:00 PM
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+float Pi=3.14159f;
+//Function Prototypes
+
+//Execution
+int main(int argc, char** argv) {
+    //Define Variables
+    float dPizza,rPizza,aPizza,nSlices,tPizzas;
+    float aSlice=14.125; //Area of single slice in inches.
+    short onePrsn=4,tPrsns;
+    //Begin output
+    cout<<"This program will calculate the number of pizzas of a given size needed for a party."<<endl;
+    cout<<"First, enter the diameter of the pizza you would like, in inches."<<endl;
+    cin>>dPizza;  //Prompt for input of pizza size
+    cout<<"Next, enter how many people will be attending this party."<<endl;
+    cin>>tPrsns;  //Prompt for input of total attendees
+    //Calculations
+    rPizza=dPizza/2.0;
+    aPizza=Pi * pow(rPizza, 2.0);
+    nSlices=aPizza/aSlice;
+    tPizzas=(onePrsn/nSlices)* tPrsns;
+    //Output
+    cout<<fixed<<setprecision(1)<<showpoint<<endl; //Formatting of output
+    cout<<"The number of pizzas of this diameter that will be needed is: "<<tPizzas<<" pizzas."<<endl;
+    //exit
+    return 0;
+}
+
