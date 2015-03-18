4c57ab74ed818c337b3a8f2f0f349c5105e68b5e
spyroshav <shavadjia@student.rcc.edu>
2015-03-17 22:13:04 -0700

    Signed-off-by: spyroshav <shavadjia@student.rcc.edu>

---
+++ Hmwk/Assignment_2/Gaddis_8thEd_Ch3_Prob22_WordGame/main.cpp
@@ -0,0 +1,48 @@
+/* 
+ * File:   main.cpp
+ * Author: Spyros Havadjia
+ *      Purpose: Assignment 2
+ *          Word Game
+ * Created on March 17, 2015, 5:09 PM
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Properties
+
+//Execution
+
+int main(int argc, char** argv) {
+    //Define Variables
+    string uName,uCity,uCllge,uPrfsn,uAnim,uPName;
+    unsigned short uAge;
+    //Output
+    cout<<"This is a Word Game Program! Lets put together a fun story together!"<<endl;
+    cout<<"First, what's your name?"<<endl;
+    getline(cin,uName);
+    cout<<"Next, how old are you?"<<endl;
+    cin>>uAge;
+    cin.ignore();
+    cout<<"Name a city you love!"<<endl;
+    getline(cin,uCity);
+    cout<<"Name a college of that city, or of a neighboring city."<<endl;
+    getline(cin,uCllge);
+    cout<<"And what profession do they specialize in preparing you for at that college?"<<endl;
+    getline(cin,uPrfsn);
+    cout<<"Name a type of pet people in that city would be likely to own."<<endl;
+    getline(cin,uAnim);
+    cout<<"Finally, what name would YOU give to such a pet?"<<endl;
+    getline(cin,uPName);
+    cout<<endl;
+    cout<<"Fantastic! Now that we've learned all that, here's how our story goes:"<<endl<<endl;
+    
+    cout<<"There once was a person named "<<uName<<" who lived in "<<uCity<<". At the age of "<<endl<<uAge<<", "<<uName<<" went to college at "<<uCllge<<". "<<uName<<" graduated and went to work "<<endl<<"as a "<<uPrfsn<<". Then, "<<uName<<" adopted a(n) "<<uAnim<<" named "<<uPName<<". They "<<endl<<"both lived happily ever after!"<<endl;
+    return 0;
+}
+
--- Hmwk/Assignment_2/Gaddis_8thEd_Ch3_Prob22_WordGame/nbproject/Package-Debug.bash
+++ Hmwk/Assignment_2/Gaddis_8thEd_Ch3_Prob22_WordGame/nbproject/Package-Debug.bash
@@ -13,9 +13,9 @@ CND_BUILDDIR=build
 CND_DLIB_EXT=dll
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/gaddis_8thed_ch3_prob13_currency
-OUTPUT_BASENAME=gaddis_8thed_ch3_prob13_currency
-PACKAGE_TOP_DIR=gaddis8thedch3prob13currency/
+OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/gaddis_8thed_ch3_prob22_wordgame
+OUTPUT_BASENAME=gaddis_8thed_ch3_prob22_wordgame
+PACKAGE_TOP_DIR=gaddis8thedch3prob22wordgame/
 
 # Functions
 function checkReturnCode
@@ -60,15 +60,15 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/gaddis8thedch3prob13currency/bin"
+makeDirectory "${NBTMPDIR}/gaddis8thedch3prob22wordgame/bin"
 copyFileToTmpDir "${OUTPUT_PATH}.exe" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}.exe" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/gaddis8thedch3prob13currency.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/gaddis8thedch3prob22wordgame.tar
 cd ${NBTMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/gaddis8thedch3prob13currency.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/gaddis8thedch3prob22wordgame.tar *
 checkReturnCode
 
 # Cleanup
