55d405855b5ce7ed5e6b053eea4839a232e757bc
spyroshav <shavadjia@student.rcc.edu>
2015-03-23 10:55:23 -0700

    Signed-off-by: spyroshav <shavadjia@student.rcc.edu>

---
+++ Hmwk/Assignment_3/Gaddis_8thEd_Ch4_Prob14_RunningTheRace/main.cpp
@@ -0,0 +1,37 @@
+/* 
+ * File:   main.cpp
+ * Author: Spyros Havadjia
+ *      Purpose: Assignment 3
+ *              Running the Race
+ * Created on March 23, 2015, 10:42 AM
+ */
+
+//Sstem Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution
+int main(int argc, char** argv) {
+//Define Variables
+    short rnnr1,rnnr2,rnnr3;
+    int tme1,tme2,tme3;
+//Begin display
+    <<cout"This program calculates 1st 2nd and 3rd place for specified runners"<<endl<<" of te race"<<endl;
+//Prompt for inputs
+    cout<<"Enter the name and time of the first runner(Separate by spacing)."<<endl;
+    cin>>rnnr1>>tme1;
+    cout<<"Enter the name and time of the second runner(Separate by spacing)."<<endl;
+    cin>>rnnr2>>tme2;
+    cout<<"Enter the name and time of the third runner(Separate by spacing)."<<endl;
+    cin>>rnnr3>>tme3;
+//Begin evalutaing & Outputting.
+    
+    return 0;
+}
+
