a58d3f175648311e48c6284aac1384250f64f58d
spyroshav <shavadjia@student.rcc.edu>
2015-04-22 16:18:37 -0700

    Signed-off-by: spyroshav <shavadjia@student.rcc.edu>

--- Hmwk/Assignment_4/Gaddis_8thEd_Ch5_Prob1_SumOfNumbers/nbproject/Makefile-variables.mk
+++ Hmwk/Assignment_4/Gaddis_8thEd_Ch5_Prob1_SumOfNumbers/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
 CND_ARTIFACT_NAME_Debug=gaddis_8thed_ch5_prob1_sumofnumbers
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/gaddis_8thed_ch5_prob1_sumofnumbers
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/gaddis_8thed_ch5_prob1_sumofnumbers
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
 CND_PACKAGE_NAME_Debug=gaddis8thedch5prob1sumofnumbers.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/gaddis8thedch5prob1sumofnumbers.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/gaddis8thedch5prob1sumofnumbers.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
 CND_ARTIFACT_NAME_Release=gaddis_8thed_ch5_prob1_sumofnumbers
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/gaddis_8thed_ch5_prob1_sumofnumbers
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/gaddis_8thed_ch5_prob1_sumofnumbers
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
 CND_PACKAGE_NAME_Release=gaddis8thedch5prob1sumofnumbers.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/gaddis8thedch5prob1sumofnumbers.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/gaddis8thedch5prob1sumofnumbers.tar
 #
 # include compiler specific variables
 #
